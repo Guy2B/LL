b3bfcfaf94ae55ab34fda090c5f67a77b97d4cc4
Guy2B <guybeaho@gmail.com>
2026-03-31 11:46:43 +0200

    Delete assets/js directory

--- assets/js/nav-mobile.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-toggle.addEventListener('click', () => {
-  toggle.classList.toggle('active');
-  navLinks.classList.toggle('open');
-});
